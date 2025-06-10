fc83aae214d25364ed673b48d38633721e215990
Mottakin890 <mottakinulalam@gmail.com>
2025-06-10 14:04:58 +0600

    Variable declaration & initialization...

---
+++ dart_practice/bin/_1st.dart
@@ -0,0 +1,9 @@
+//variable declaration and initialization
+// This Dart program declares two variables, initializes them with values,
+
+void main(){
+  var x = 10;
+  var y = 20;
+  var sum = x + y;
+  print("The sum of $x and $y is $sum");
+}
