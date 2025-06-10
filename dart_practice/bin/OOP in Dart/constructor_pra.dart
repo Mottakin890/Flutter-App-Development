0a23adb3d7bf5a46b70ca32cbd2d60045e9710ec
Mottakin890 <mottakinulalam@gmail.com>
2025-06-11 00:20:27 +0600

    Dart oop class constructor and constructor parameter..

---
+++ dart_practice/bin/OOP in Dart/constructor_pra.dart
@@ -0,0 +1,5 @@
+class MyNewClass{
+  MyNewClass(String msg){
+    print(msg);
+  }
+}
---
+++ dart_practice/bin/OOP in Dart/main_class.dart
@@ -0,0 +1,4 @@
+import 'constructor_pra.dart';
+void main(){
+  var obj = MyNewClass("I am a constructor parameter.");
+}
